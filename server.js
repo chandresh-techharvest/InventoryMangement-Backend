80f784ca4a1f553fdc9e8364e91be145a0e3e12f
Ptiwari088 <Pragyanshu.tiwari@thetechharvest.com>
2026-02-17 15:04:22 +0530

    fix cors and cookie settings for production

--- server.js
+++ server.js
@@ -30,7 +30,9 @@ app.use(cors({
             callback(new Error('Not allowed by CORS'));
         }
     },
-    credentials: true
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 app.use(cookieParser());
